bbd802e7aaa309a90313032e118331b6fa8a176a
Scott Hyndman <scott@madewithfelt.com>
2022-02-02 22:45:27 -0500

    Scaffold a new package

---
+++ example/stack_attack_example.dart
@@ -0,0 +1,6 @@
+import 'package:stack_attack/stack_attack.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/stack_attack_test.dart
@@ -0,0 +1,16 @@
+import 'package:stack_attack/stack_attack.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
