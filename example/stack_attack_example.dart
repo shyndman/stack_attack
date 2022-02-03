893a7b5e83395fded33d35c4091e372be3606ae5
Scott Hyndman <scott@madewithfelt.com>
2022-02-02 22:50:18 -0500

    Tidy scaffolding

--- example/stack_attack_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:stack_attack/stack_attack.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
--- test/stack_attack_test.dart
+++ test/stack_attack_test.dart
@@ -1,16 +1,1 @@
-import 'package:stack_attack/stack_attack.dart';
-import 'package:test/test.dart';
-
-void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
-
-    setUp(() {
-      // Additional setup goes here.
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
-  });
-}
+void main() {}
